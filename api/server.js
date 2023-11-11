ecde507c2ac10ceb74cc99bc9ee5dc46685fd236
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-11-10 23:10:48 -0300

    enable write operations

--- api/server.js
+++ api/server.js
@@ -4,15 +4,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
